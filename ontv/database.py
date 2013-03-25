9b9ae9e269e256baec3c034f39de2a2d1e437c8e
John-John Tedro <johnjohn.tedro@gmail.com>
2013-03-25 20:05:54 +0000

    datbase: version designator and a migratable codebase

--- ontv/database.py
+++ ontv/database.py
@@ -1,10 +1,39 @@
-import json
 import contextlib
+import json
 import os
 
 
-class JsonBlockFormat(object):
-    def load(self, v):
+class Block(object):
+    def load_value(self, v):
+        raise NotImplementedError("load_value")
+
+    def dump_value(self, v):
+        raise NotImplementedError("dump_value")
+
+    def load_key(self, key):
+        return key
+
+    def dump_key(self, key):
+        return key
+
+    def write_header(self, fd):
+        raise NotImplementedError("write_header")
+
+    def read_header(self, fd):
+        raise NotImplementedError("read_header")
+
+    def load_item(self, i):
+        raise NotImplementedError("load_item")
+
+    def write_entry(self, fd, op, key, value):
+        raise NotImplementedError("write_entry")
+
+    def parse_entry(self, line):
+        raise NotImplementedError("parse_entry")
+
+
+class JSONValueBlock(object):
+    def load_value(self, v):
         if v == "":
             return None
 
@@ -13,7 +42,7 @@ class JsonBlockFormat(object):
         except:
             return None
 
-    def dump(self, v):
+    def dump_value(self, v):
         if v is None:
             return ""
 
@@ -23,116 +52,162 @@ class JsonBlockFormat(object):
             return ""
 
 
-class FilesystemDriver(object):
+class StandardHeader(object):
+    def write_header(self, fd):
+        fd.write("version={0}".format(self.VERSION) + self.DELIM)
+
+    def read_header(self, fd):
+        v = fd.readline()
+        v = v.strip()
+        return dict(s.split("=", 1) for s in v.split(" "))
+
+
+class V1Block(JSONValueBlock, StandardHeader, Block):
     SPACE = " "
     DELIM = "\n"
+    VERSION = '1.0'
 
-    def __init__(self, path, driver_open=open):
+    def __init__(self, encoding='utf-8'):
+        self._encoding = encoding
+
+    def load_item(self, i):
+        key, value = i
+        return (self.load_key(key), self.load_value(value))
+
+    def write_entry(self, fd, op, key, value):
+        key = self.dump_key(key).encode(self._encoding)
+        value = self.dump_value(value).encode(self._encoding)
+        key_length = str(len(key))
+        entry = self.SPACE.join((op, key_length, key, value)) + self.DELIM
+        fd.write(entry)
+
+    def parse_entry(self, line):
+        op, key_length, b = line.split(self.SPACE, 2)
+        key_length = int(key_length)
+        key = b[:key_length].decode(self._encoding)
+        value = b[key_length + 1:-1].decode(self._encoding)
+        return op, key, value
+
+
+class BlockYielder(object):
+    def __init__(self, block_format, fd):
+        self._block = block_format
+        self._fd = fd
+
+    def readblocks(self):
+        if self._fd is None:
+            raise Exception("file not open")
+
+        try:
+            for line in self._fd:
+                op, key, value = self._block.parse_entry(line)
+                key = self._block.load_key(key)
+                yield op, key, value
+        finally:
+            self._fd.close()
+            self._fd = None
+
+    def load_item(self, i):
+        return self._block.load_item(i)
+
+
+class FilesystemDriver(object):
+    block_formats = {
+        V1Block.VERSION: V1Block,
+    }
+
+    def __init__(self, path, driver_open=open, encoding='utf-8'):
         self._path = path
         self._compact_path = os.path.join(
             os.path.dirname(path), u".{0}.compact".format(
                 os.path.basename(path)))
         self._driver_open = driver_open
-
-    def _format_entry(self, op, ident, block):
-        return self.SPACE.join((op, ident, block)) + self.DELIM
-
-    def open(self):
-        self._fd = self._driver_open(self._path, 'a+')
-
-    def close(self):
-        if not self._fd:
-            raise Exception("file not open")
-
-        self._fd.close()
-        self._fd = None
-
-    def reopen(self):
-        if self._fd:
-            self._fd.close()
-
-        self._fd = self._driver_open(self._path, 'a+')
+        self._encoding = encoding
+        self._block = V1Block(encoding=self._encoding)
 
     def compact(self, entries):
-        if not self._fd:
-            raise Exception("file not open")
-
         with self._driver_open(self._compact_path, 'w') as fd:
-            for op, ident, block in entries:
-                fd.write(self._format_entry(op, ident, block))
+            self._block.write_header(fd)
+
+            for op, key, value in entries:
+                self._block.write_entry(fd, op, key, value)
 
         os.rename(self._compact_path, self._path)
-        self.reopen()
 
-    def append(self, op, ident, block):
-        self._fd.write(self._format_entry(op, ident, block))
+    def appendlog(self, log):
+        if not log:
+            return
 
-    def yieldentries(self):
-        if not self._fd:
-            raise Exception("file not open")
+        with open(self._path, 'a') as fd:
+            if fd.tell() == 0:
+                self._write_header(fd)
 
-        self._fd.seek(0)
+            for op, key, value in log:
+                fd.write(self._format_entry(op, key, value))
 
-        for line in self._fd:
-            op, ident, block = line.split(self.SPACE, 2)
-            yield op, ident, block
+    def createyielder(self):
+        fd = self._driver_open(self._path, 'r')
 
-    def valid_id(self, ident):
-        if self.SPACE in ident:
-            raise Exception("identifier may not contain a space")
+        header = self._block.read_header(fd)
 
-        if self.DELIM in ident:
-            raise Exception("identifier may not contain a newline")
+        # setup temporary block format for reading.
+        # this is useful for migrating old database formats behind the scenes.
+        # The next time the database is compacted, the block format will
+        # change.
+        block_format = self.block_formats.get(header['version'])
 
-    def db_size(self):
-        return self._fd.size()
+        if block_format is None:
+            raise Exception(
+                "unknown block format version: {0}".format(header['version']))
+
+        block_format = block_format(encoding=self._encoding)
+
+        return BlockYielder(block_format, fd)
 
 
 class DictStorage(object):
-    PUT = "+"
-    REMOVE = "-"
+    # add a single value.
+    VALUE = "+"
+
+    # delete a single value.
+    DELETION = "-"
+
+    # clear the entire data set.
     CLEAR = "X"
 
-    def __init__(self, path, driver, block):
+    def __init__(self, path, driver):
         self._path = path
-        self._fd = None
         self._log = list()
         self._driver = driver
-        self._block = block
         self._statistics = dict()
 
     def open(self):
-        self._driver.open()
-        self._fd = open(self._path, 'a+')
-        cache = self._read_cache(self._driver.yieldentries())
+        cache = self._read_cache(self._driver.createyielder())
         return cache, self._statistics
 
     def statistics(self):
         return self._statistics
 
-    def _read_cache(self, entries):
-        if not self._fd:
-            raise Exception("file not open")
-
+    def _read_cache(self, yielder):
         data = dict()
 
         # generate statistics to decide if we should autocompact the database.
         clears = 0
         removes = 0
 
-        for op, ident, block in entries:
-            if op == self.REMOVE:
-                data.pop(ident, None)
+        for op, key, value in yielder.readblocks():
+            if op == self.DELETION:
+                data.pop(key, None)
                 removes += 1
-                continue
-
-            if op == self.PUT:
-                data[ident] = block
                 continue
 
             if op == self.CLEAR:
                 data.clear()
                 clears += 1
+                continue
+
+            if op == self.VALUE:
+                data[key] = value
                 continue
 
             raise Exception("unknown operation '{0}'".format(op))
@@ -143,35 +218,24 @@ class DictStorage(object):
             "nops": removes + clears,
         }
 
-        return (dict((k, self._block.load(v)) for k, v in data.items()))
-
-    def _append_log(self, log):
-        for op, ident, block in log:
-            block = self._block.dump(block)
-            self._driver.append(op, ident, block)
+        return dict(yielder.load_item(i) for i in data.items())
 
     def commit(self):
-        self._append_log(self._log)
+        self._driver.appendlog(self._log)
         self._log = list()
 
-    def close(self):
-        self._driver.close()
+    def setitem(self, key, value):
+        self._log.append((self.VALUE, key, value))
 
-    def valid_id(self, ident):
-        return self._driver.valid_id(ident)
+    def delitem(self, key):
+        self._log.append((self.DELETION, key, None))
 
-    def setitem(self, ident, block):
-        self._log.append((self.PUT, ident, block))
-
-    def delitem(self, ident):
-        self._log.append((self.REMOVE, ident, None))
-
-    def clear(self, ident):
+    def clear(self, key):
         self._log.append((self.CLEAR, "", None))
 
     def compact(self, items):
-        generator = ((self.PUT, ident, self._block.dump(block))
-                     for ident, block in items)
+        generator = ((self.VALUE, key, value) for key, value in items)
+
         self._driver.compact(generator)
 
         statistics = self._statistics
@@ -193,20 +257,17 @@ class DictDB(dict):
         self._storage = storage
         dict.__init__(self, cache)
 
-    def __setitem__(self, ident, data):
-        self._storage.valid_id(ident)
-        dict.__setitem__(self, ident, data)
-        self._storage.setitem(ident, data)
+    def __setitem__(self, key, data):
+        dict.__setitem__(self, key, data)
+        self._storage.setitem(key, data)
 
-    def __delitem__(self, ident):
-        self._storage.valid_id(ident)
-        dict.__delitem__(self, ident)
-        self._storage.delitem(ident)
+    def __delitem__(self, key):
+        dict.__delitem__(self, key)
+        self._storage.delitem(key)
 
-    def pop(self, ident, *args, **kw):
-        self._storage.valid_id(ident)
-        value = dict.pop(self, ident, *args, **kw)
-        self._storage.delitem(ident)
+    def pop(self, key, *args, **kw):
+        value = dict.pop(self, key, *args, **kw)
+        self._storage.delitem(key)
         return value
 
     def clear(self):
@@ -222,15 +283,15 @@ class DictDB(dict):
     def popitem(self, **args):
         raise NotImplementedError("popitem")
 
-    def list_append(self, ident, data):
-        array = list(self.get(ident, []))
+    def list_append(self, key, data):
+        array = list(self.get(key, []))
         array.append(data)
-        self.__setitem__(ident, array)
+        self.__setitem__(key, array)
 
-    def list_remove(self, ident, data):
-        array = self.get(ident, [])
+    def list_remove(self, key, data):
+        array = self.get(key, [])
         array.remove(data)
-        self.__setitem__(ident, array)
+        self.__setitem__(key, array)
 
     def compact(self):
         return self._storage.compact(self.items())
@@ -244,23 +305,21 @@ class SetDB(set):
         self._storage = storage
         set.__init__(self, cache.keys())
 
-    def add(self, ident):
-        if ident in self:
+    def add(self, key):
+        if key in self:
             return
 
-        self._storage.valid_id(ident)
-        set.add(self, ident)
-        self._storage.setitem(ident, None)
+        set.add(self, key)
+        self._storage.setitem(key, None)
 
-    def remove(self, ident):
-        self._storage.valid_id(ident)
-        set.remove(self, ident)
-        self._storage.delitem(ident)
+    def remove(self, key):
+        set.remove(self, key)
+        self._storage.delitem(key)
 
     def pop(self):
-        ident = set.pop(self)
-        self._storage.delitem(ident)
-        return ident
+        key = set.pop(self)
+        self._storage.delitem(key)
+        return key
 
     def compact(self):
         return self._storage.compact((v, None) for v in self)
@@ -276,12 +335,10 @@ def open_database(
     impl=DictDB,
     driver_open=open,
     driver=FilesystemDriver,
-    block_format=JsonBlockFormat
 ):
     driver_instance = driver(path, driver_open=driver_open)
-    block = block_format()
 
-    storage = DictStorage(path, driver_instance, block)
+    storage = DictStorage(path, driver_instance)
 
     cache, statistics = storage.open()
 
@@ -295,8 +352,5 @@ def open_database(
     if statistics['nops'] > compaction_limit:
         db.compact()
 
-    try:
-        yield db
-        storage.commit()
-    finally:
-        storage.close()
+    yield db
+    storage.commit()
