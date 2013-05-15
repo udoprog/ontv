56ee6cea305d51310c8e605f6641a6e9c415b2e6
John-John Tedro <johnjohn.tedro@gmail.com>
2013-05-15 18:15:33 +0000

    release 0.7.2

--- ontv/__init__.py
+++ ontv/__init__.py
@@ -23,7 +23,7 @@ from .utils import read_yaml
 
 from .database import open_database
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 log = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-VERSION = '0.7.1'
+VERSION = '0.7.2'
 
 
 setup(
